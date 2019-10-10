633a502707c65da56f6f89cfb2ece70d34553656
696499 <696499@pdsb.net>
2019-10-10 09:17:00 -0400

    js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
-function myFunction() {
-    alert('Hello! Write your first angle.');
+function myFunction(){
+	alert("hello");
+	let a1 = parseInt (prompt("enter first angle"));
+	let a2 = parseInt (prompt("enter second angle"));
+	let a3 = 180 - (a1 + a2);
+	alert("The third angle is:" + a3);
 }
