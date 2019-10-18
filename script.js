33bf8974031e3ec48e92500e54fb3070f02de1f5
696499 <696499@pdsb.net>
2019-10-18 09:33:46 -0400

    assignments

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 
-function myFunction(){
-	alert("hello");
-	let a1 = parseInt (prompt("enter first angle"));
-	let a2 = parseInt (prompt("enter second angle"));
-	let a3 = 180 - (a1 + a2);
-	alert("The third angle is:" + a3);
+ function myFunction(){
+ 	alert("hello");
+ 	let a1 = parseInt (prompt("enter first angle"));
+ 	let a2 = parseInt (prompt("enter second angle"));
+ 	let a3 = 180 - (a1 + a2);
+ 	alert("The third angle is:" + " " + a3);
+ }
+
+  function century(){
+ 	let year = parseInt (prompt("Write a year and I will tell you the century it belongs in!"));
+ 	let century1 = parseInt((year - 1) / 100) + 1;
+ 	alert("The century is" + " " + century1);
+
+ }
+
+function integers(){
+	alert("check the console");
+
+	for (let i = 1; i < 101; i++){
+	
+		if (i % 4 == 0){
+	      console.log("Four");
+	    }
+	    else if (i % 10 == 0){
+	      console.log("Ten");
+	    }
+	    else if (i % 10 && 4 == 0){
+	    	console.log("TenFour");
+	    }
+	    else {
+	    	console.log(i);
+	    }
+	}
 }
